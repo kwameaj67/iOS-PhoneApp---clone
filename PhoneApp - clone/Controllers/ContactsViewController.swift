161bb7bf1424f720ff4ec29961ff9743aa6e81cf
Kwame <kwameagyenimboateng64@gmail.com>
2021-09-23 18:29:10 +0000

    add searchBar

--- PhoneApp - clone/Controllers/ContactsViewController.swift
+++ PhoneApp - clone/Controllers/ContactsViewController.swift
@@ -3,22 +3,27 @@
 //  PhoneApp - clone
 //
 //  Created by Kwame Agyenim - Boateng on 06/05/2021.
-//
+// https://stackoverflow.com/questions/33973574/fetching-all-contacts-in-ios-swift
 
 import UIKit
 
-class ContactsViewController: UIViewController {
-
+class ContactsViewController: UIViewController{
+    let searchBarController : UISearchController = {
+        let searchBar = UISearchController()
+        return searchBar
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Contacts"
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Groups", style:.plain, target: self, action: #selector(createGroup))
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem:.add, target: self, action: #selector(addContact))
-       
-        
+        navigationItem.searchController = searchBarController
+        configureSearchBar()
       
     }
+   
     @objc func createGroup(){
         
     }
@@ -27,6 +32,17 @@ class ContactsViewController: UIViewController {
         
     }
 
-   
+}
 
+extension ContactsViewController: UISearchControllerDelegate, UISearchBarDelegate {
+    func configureSearchBar(){
+        searchBarController.delegate = self
+        searchBarController.obscuresBackgroundDuringPresentation = false
+        searchBarController.searchBar.delegate = self
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        print(searchBar.text!)
+
+    }
 }
